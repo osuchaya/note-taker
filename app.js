e453e6e8bfd85ca629f9f7c12a54d970fb00a754
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-24 18:18:21 -0700

    Added app use functions to app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3001;
+const pFolder = path.join(__dirname, "/public");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(port, function () {
     console.log(`App listening on port ${port}.`);
