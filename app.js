4a265479aed9af4ac4499284f2d209a318da68e4
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-24 18:41:35 -0700

    read existing post from db then add new note to dband send response back to front-end

--- app.js
+++ app.js
@@ -12,22 +12,31 @@ app.use(express.json());
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(pFolder, "notes.html"));
-  });
-
-  app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-  });
-
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(pFolder, "index.html"));
-  });
-
-  app.post("/api/notes", function (req, res) {
-    console.log('Note received for creation');
-    console.log('req.body = ',req.body);
-    
 });
-  
+
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(pFolder, "index.html"));
+});
+
+app.post("/api/notes", function (req, res) {
+    console.log('Note received for creation');
+    console.log('req.body = ', req.body);
+
+    let notesInDb = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let createNote = req.body;
+
+    let createID = notesInDb.length.toString();
+    createNote.id = createID;
+    notesInDb.push(createNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesInDb));
+    res.json(notesInDb);
+});
+
 app.listen(port, function () {
     console.log(`App listening on port ${port}.`);
-  });
+});
