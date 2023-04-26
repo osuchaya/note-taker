c0f5fdf5ae62dcb8b24505416695e45fa65a54ea
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-25 17:37:08 -0700

    added comments

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const express = require("express");
+// const uuid = require("uuid");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,28 +11,31 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(pFolder, "notes.html"));
+app.get("/notes", function (req, res) {   // get /notes from front-end (notes.html)
+    res.sendFile(path.join(pFolder, "notes.html")); //send notes.html to front-end. 
 });
 
-app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+app.get("/api/notes", function (req, res) {     // got /api/notes from front-end
+    res.sendFile(path.join(__dirname, "/db/db.json")); //send /notes available in db.json to front-end
 });
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(pFolder, "index.html"));
+app.get("*", function (req, res) {                  //catch-all other than /api/notes received
+    res.sendFile(path.join(pFolder, "index.html"));  //send index.html to front-end
 });
 
-app.post("/api/notes", function (req, res) {
+app.post("/api/notes", function (req, res) {         //got post request for route /api/notes from front-end
     console.log('Note received for creation');
-    console.log('req.body = ', req.body);
+    console.log('req.body = ', req.body);            //req.body = new notes
 
-    let notesInDb = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let createNote = req.body;
+    let notesInDb = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));    //read all current notes from db.json and store in an array
+    let createNote = req.body;                       //create a new object and store body contents there
 
     let createID = notesInDb.length.toString();
+    // let createID = uuid()
     createNote.id = createID;
     notesInDb.push(createNote);
+
+
 
     fs.writeFileSync("./db/db.json", JSON.stringify(notesInDb));
     res.json(notesInDb);
